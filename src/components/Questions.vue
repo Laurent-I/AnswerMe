d76a339e739d13369b3ae18fe527583cd0413042
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-09 05:08:05 +0200

    App Components

---
+++ src/components/Questions.vue
@@ -0,0 +1,47 @@
+<!--<template>-->
+<!--  <div class="questions-ctr">-->
+<!--    <div class="progress">-->
+<!--&lt;!&ndash;      Binding Dynamic progress bar Styling&ndash;&gt;-->
+<!--      <div class="bar" :style="{width:`${(questionsAnswered/questions.length) * 100}%`}"></div>-->
+<!--      <div class="status">{{questionsAnswered}} out of {{questions.length}} questions answered</div>-->
+<!--    </div>-->
+<!--    <transition-group name="fade">-->
+<!--      <div-->
+<!--        class="single-question"-->
+<!--        v-for="(question, qi) in questions"-->
+<!--        :key="question.q"-->
+<!--        v-show="questionsAnswered === qi">-->
+<!--        <div class="question">{{question.q}}</div>-->
+<!--        <div class="answers">-->
+<!--          <div-->
+<!--            class="answer"-->
+<!--            v-for="answer in question.answers"-->
+<!--            :key="answer.text"-->
+<!--            @click.prevent="selectAnswer(answer.is_correct)"-->
+<!--          >-->
+<!--            {{answer.text}}-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </transition-group>-->
+
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "QuestionsComponent",-->
+<!--  props:["questions", "questionsAnswered"],-->
+<!--  emits:['question-answered'],-->
+<!--  methods:{-->
+<!--    //Selecting answer emitter function-->
+<!--    selectAnswer(is_correct){-->
+<!--      this.$emit('question-answered', is_correct)-->
+<!--    }-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
---
+++ src/components/Result.vue
@@ -0,0 +1,34 @@
+<!--&lt;!&ndash;Result Component&ndash;&gt;-->
+<!--<template>-->
+<!--  <div class="result">-->
+<!--    &lt;!&ndash;Rendering data dynamically &ndash;&gt;-->
+<!--    <div class="title">{{results[resultIndex].title}}</div>-->
+<!--    <div class="desc">-->
+<!--      {{results[resultIndex].desc}}-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "ResultComponent",-->
+<!--  props:["results", "totalCorrect"],-->
+<!--  computed:{-->
+<!--    resultIndex(){-->
+<!--      let index = 0;-->
+
+<!--      this.results.forEach((e,i)=>{-->
+<!--        if (e.min <= this.totalCorrect && e.max >= this.totalCorrect){-->
+<!--          index = i;-->
+<!--        }-->
+<!--      })-->
+
+<!--      return index;-->
+<!--    }-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
