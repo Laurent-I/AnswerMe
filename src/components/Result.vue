2254fb1f17d06c1b9250f808c635e7b0e6bf8096
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-09 05:15:57 +0200

    Result Component

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,34 +1,34 @@
-<!--&lt;!&ndash;Result Component&ndash;&gt;-->
-<!--<template>-->
-<!--  <div class="result">-->
+<!--Result Component-->
+<template>
+  <div class="result">
 <!--    &lt;!&ndash;Rendering data dynamically &ndash;&gt;-->
 <!--    <div class="title">{{results[resultIndex].title}}</div>-->
 <!--    <div class="desc">-->
 <!--      {{results[resultIndex].desc}}-->
 <!--    </div>-->
-<!--  </div>-->
-<!--</template>-->
+  </div>
+</template>
 
-<!--<script>-->
-<!--export default {-->
-<!--  name: "ResultComponent",-->
-<!--  props:["results", "totalCorrect"],-->
-<!--  computed:{-->
-<!--    resultIndex(){-->
-<!--      let index = 0;-->
+<script>
+export default {
+  name: "ResultComponent",
+  props:["results", "totalCorrect"],
+  computed:{
+    resultIndex(){
+      let index = 0;
 
-<!--      this.results.forEach((e,i)=>{-->
-<!--        if (e.min <= this.totalCorrect && e.max >= this.totalCorrect){-->
-<!--          index = i;-->
-<!--        }-->
-<!--      })-->
+      this.results.forEach((e,i)=>{
+        if (e.min <= this.totalCorrect && e.max >= this.totalCorrect){
+          index = i;
+        }
+      })
 
-<!--      return index;-->
-<!--    }-->
-<!--  }-->
-<!--};-->
-<!--</script>-->
+      return index;
+    }
+  }
+};
+</script>
 
-<!--<style scoped>-->
+<style scoped>
 
-<!--</style>-->
+</style>
